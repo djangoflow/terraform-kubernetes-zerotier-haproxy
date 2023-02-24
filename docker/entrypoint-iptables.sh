049b58457bb04b4ef809b05c7e9ee2a2078e0333
Alexis Yushin <alexis@apexive.com>
2023-02-23 23:14:32 -0400

    bug: fix Dockerfile and entrypoint

--- docker/entrypoint-iptables.sh
+++ docker/entrypoint-iptables.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-if "$MASQUERADE" == "true"
+#!/usr/bin/env sh
+if [ "$MASQUERADE" = "true" ]
 then
   COMMAND="iptables-legacy -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
   echo "=> $COMMAND"
